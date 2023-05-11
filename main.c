040a18b8a2f3e218933d05f4d7ac2f077366d3ce
salllma <salma.wagdy@hotmail.com>
2023-05-12 00:18:10 +0200

    add index

--- main.c
+++ main.c
@@ -6,22 +6,22 @@ const int CROSSES = 2;
 const int EMPTY = 0;
 const int BOARDER = 1;
 
-const int ConvertTo25[] = 
-{
-	6,7,8,
-	11,12,13,
-	16,17,18
-};
+
 
 void Init_board(int board[][5])
 {
     int i, j;
 
-    for (i = 0; i < 5; ++i) {
-        for (j = 0; j < 5; ++j) {
-            if (i == 0 || i == 4 || j == 0 || j == 4) {
+    for (i = 0; i < 5; ++i) 
+    {
+        for (j = 0; j < 5; ++j)
+         {
+            if (i == 0 || i == 4 || j == 0 || j == 4) 
+            {
                 board[i][j] = BOARDER;
-            } else {
+            } 
+            else 
+            {
                 board[i][j] = EMPTY;
             }
         }
